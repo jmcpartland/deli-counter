d244d111e7766e059dc8d7a935394a91214a8010
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-02-09 09:21:16 -0700

    commit tree 6613c85396a628a1cee6959b0efcf891cbfd781e

--- test.rb
+++ test.rb
@@ -1,28 +1,29 @@
+#katz_deli = []
+katz_deli = ["Tom", "Joe", "Jim"]
+
+
 def line(katz_deli)
     if katz_deli.empty?
         puts "The line is currently empty."
     else
-        customers = "The line is currently:"
+        customers = "The line is currently: "
         katz_deli.each.with_index(1) do |person, number|
-            customers << " #{number}. #{person}"
+            customers << "#{number}. #{person}"
         end
-        puts customers
+        puts "#{customers}"
     end
 
 end
 
+line(katz_deli)
+
+
+name = "Ada"
 
 def take_a_number(katz_deli, name)
     katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
 
+take_a_number(katz_deli, name)
 
-def now_serving(katz_deli)
-    if katz_deli.empty?
-        puts "There is nobody waiting to be served!"
-    else
-        puts "Currently serving #{katz_deli[0]}."
-        katz_deli.shift
-    end
-end
